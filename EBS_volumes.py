5cf2335446578f69025e48ae5aca8b40810c97ee
vinaygwd <vinaygwd4@gmail.com>
2019-03-07 18:18:05 +0530

    Added Heading

--- EBS_volumes.py
+++ EBS_volumes.py
@@ -1,6 +1,7 @@
 import boto3
 client = boto3.client('ec2')
 volume = client.describe_volumes()
+print("Volume","   |   ","State","|","AZ","|","Size","|","Iops")
 for i in volume['Volumes']:
     if i['Attachments'] is not None:
         print(i['VolumeId'],"|",i['State'],"|",i['AvailabilityZone'],"|",i['Size'],"|",i['Iops'])
