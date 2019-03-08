627462fddcc3c136fca3d248660e83f58bb98247
vinaygwd <vinaygwd4@gmail.com>
2019-03-08 10:51:35 +0530

    Update EBS_volumes.py

--- EBS_volumes.py
+++ EBS_volumes.py
@@ -2,5 +2,8 @@ import boto3
 client = boto3.client('ec2')
 volume = client.describe_volumes()
 for i in volume['Volumes']:
-    if i['Attachments'] is not None:
-        print(i['VolumeId'],"|",i['State'],"|",i['AvailabilityZone'],"|",i['Size'],"|",i['Iops'])
+    if i['Attachments'] != []:
+        if 'InstanceId' in i['Attachments'][0]:
+            print(i['Attachments'][0]['InstanceId'],"|",i['VolumeId'],"|",i['State'],"   |",i['Iops'],"|",i['Size'])
+    else:
+        print("                   ","|",i['VolumeId'],"|",i['State'],"|",i['Iops'],"|",i['Size'])
